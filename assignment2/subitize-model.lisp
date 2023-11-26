88618fc9cad3f8014c74e21fc53b913e0a6009a8
not-matthias <26800596+not-matthias@users.noreply.github.com>
2023-11-26 05:58:57 -0500

    chore: add current progress

--- assignment2/subitize-model.lisp
+++ assignment2/subitize-model.lisp
@@ -2,11 +2,13 @@
 
 (define-model subitize
 
-(sgp :v t)
+(sgp :seed (123456 0))
+(sgp :v t :show-focus t :trace-detail high)
 
-(sgp :show-focus t 
-     :visual-num-finsts 10 
-     :visual-finst-span 10)
+; (sgp :v t)
+; (sgp :show-focus t 
+;      :visual-num-finsts 10 
+;      :visual-finst-span 10)
 
 (chunk-type count count step)
 (chunk-type number number next vocal-rep)
@@ -24,7 +26,83 @@
         (ten isa number number ten next eleven vocal-rep "ten")
         (eleven isa number number eleven)
         (goal isa count step start)
-        (start))
+
+
+        (start)
+        (encode isa chunk)
+        (respond isa chunk) 
+        (done isa chunk)
+)
+
+(P find-unattended-letter
+   =goal>
+      ISA         count
+      step        start
+ ==>
+   +visual-location>
+      :attended    nil
+   =goal>
+      step        find-location
+    ;   count       zero
+)
+
+
+; Attend each letter one-by-one. 
+(P attend-letter
+   =goal>
+      ISA         count
+      step        find-location
+    ;   count       =num      ; TODO: Why do we need this? 
+   =visual-location>
+   ?visual>
+      state       free
+==>
+   =goal>
+      step        encode
+   +visual>
+      cmd         move-attention
+      screen-pos  =visual-location
+;    +retrieval>
+;        number   =num    ; Retrieve number and bind to 'num'
+)
+
+; d
+(P encode-letter
+   =goal>
+      ISA         count
+      step        encode
+      count       =num
+   =visual>
+   =retrieval>
+      number    =num
+      next      =num_next   ; Bind next number to 'num_next'
+==>
+   ;; Update count and find the next letter. 
+   =goal>
+      ISA         count
+      step        find-location
+      count        =num_next
+   +visual-location>
+      :attended    nil
+)
+
+
+
+; Respond by typing the count.
+(P respond
+   =goal>
+      ISA         count
+      step        respond
+      count       =num
+   ?manual>   
+      state       free
+==>
+   =goal>
+      step        done
+   +manual>
+      cmd         press-key
+      key         =num
+)
 
 
 (goal-focus goal)
