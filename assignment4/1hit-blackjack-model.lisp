64477289bf6759bce1e4e592e0a038a92dc93940
put101 <tobiaspucher@gmail.com>
2023-12-10 19:40:53 +0100

    stuff

--- assignment4/1hit-blackjack-model.lisp
+++ assignment4/1hit-blackjack-model.lisp
@@ -1,23 +1,24 @@
 (clear-all)
 
-(define-model 1-hit-model 
+(define-model 1-hit-model
     
   ;; do not change these parameters
   (sgp :esc t :bll .5 :ol t :sim-hook "1hit-bj-number-sims" 
        :cache-sim-hook-results t :er t :lf 0)
   
   ;; adjust these as needed
-  (sgp :v nil :ans .2 :mp 10.0 :rt -60)
+  (sgp :v nil :ans .2 :mp 15 :rt -5)
+
   
   ;; This type holds all the game info 
   
   (chunk-type game-state
-     mc1 mc2 mc3  mtot mresult oc1 oc2 oc3 ostart otot oresult state)
+     mc1 mc2 mc3 mstart mtot mresult oc1 oc2 oc3 ostart otot oresult state)
   
   ;; This chunk-type should be modified to contain the information needed
   ;; for your model's learning strategy
   
-  (chunk-type learned-info  ostart action result)
+  (chunk-type learned-info  mstart ostart action)
   
   ;; Declare the slots used for the goal buffer since it is
   ;; not set in the model defintion or by the productions.
@@ -28,7 +29,7 @@
   ;; Create chunks for the items used in the slots for the game
   ;; information and state
   
-  (define-chunks win lose bust retrieving start results)
+(define-chunks win lose bust retrieving start results)
     
   ;; Provide a keyboard for the model's motor module to use
   (install-device '("motor" "keyboard"))
@@ -46,19 +47,30 @@
        state retrieving
      +retrieval>
        isa learned-info
-       ;; MC1 =c
         mstart =ms
-       ostart =os
+        ;;ostart =os
        ;; Remove any existing value from the buffer? 
      - action nil)
 
-  ;; Condition: Retrieving state, but couldn't remember a move
-  ;; Action: Reset the goal buffer and press 's'
-  (p cant-remember-game
+   (p cant-remember-game-hit
      =goal>
        isa game-state
        state retrieving
-     ;; Failed to retrieve information
+     ?retrieval>
+       buffer  failure
+     ?manual>
+       state free
+    ==>
+     =goal>
+       state nil
+     +manual>
+       cmd press-key
+       key "h")
+  
+   (p cant-remember-game-stay
+     =goal>
+       isa game-state
+       state retrieving
      ?retrieval>
        buffer  failure
      ?manual>
@@ -72,15 +84,14 @@
   
   ;; Condition: Retrieving state, and could remember a move
   ;; Action: Press the key with the action that was remembered
-  (p remember-game-was-win
+  (p remember-game
      =goal>
        isa game-state
        state retrieving
      ;; Successfully retrieved information
      =retrieval>
        isa learned-info
-       action =act ;; action for the remembered 
-       result win
+       action =act ;; action that was remembered
      ?manual>
        state free
     ==>
@@ -103,89 +114,66 @@
      ;; the best move
      @retrieval>)
 
-  (p remember-game-was-not-win
-     =goal>
-       isa game-state
-       state retrieving
-     =retrieval>
-       isa learned-info
-       action =act ;; action for the remembered 
-      - result win
-     ?manual>
-       state free
-    ==>
-     =goal>
-       state nil
-     +manual>
-       cmd press-key
-       key "s" ;; do the opposite of the remembered action because it was a loss anyways
-     
-     ;; overwrite action and its purpose is to have the production modify the chunk in a buffer
-     ;; - The difference between the overwrite and modification operators is that with the overwrite 
-     ;;   action only the slots and values specified in the overwrite action will remain in the chunk 
-     ;;   in the buffer – all other slots and values are erased
-     ;; 
-     ;; Why is this needed? To prevent that chunk from merging back into declarative memory 
-     ;; and strengthening the chunk which was retrieved. The chunk which was retrieved 
-     ;; may not be the best action to take in the current situation either because it was retrieved
-     ;; due to noise or because the model does not yet have enough experience to accurately determine 
-     ;; the best move
-     @retrieval>)  
- 
-  (p results-should-hit
+  (p results-should-hit-win
      =goal>
        isa game-state
        state results
-       mresult =outcome
+       mresult win
        mstart  =ms
        ostart  =os
-      ;  MC1 =c
+      - mc3 nil ;; did hit
      ?imaginal>
        state free
-     =retrieval> ;; there is any chunk
     ==>
-     !output! (I =outcome)
      =goal>
        state nil
-     +imaginal>
-      ;  MC1 =c 
+     +imaginal> ;; learn this combination
+     isa learned-info
        mstart =ms
        ostart =os
        action "h"
-     =retrieval>
-       isa learned-info
-       result =outcome ;; update result before harvesting
   )
 
-  (spp results-should-hit :u 10)
-
-  
-  (p results-should-stay
+  (p results-should-stay-win
      =goal>
        isa game-state
        state results
-       mresult =outcome
-      ;  MC1 =c
+       mresult win
        mstart =ms
        ostart =os
+       mc3 nil ;; did stay
      ?imaginal>
        state free
-      =retrieval> ;; there is any chunk
     ==>
-     !output! (I =outcome)
      =goal>
        state nil
      +imaginal>
-      ;  MC1 =c 
+     isa learned-info
        mstart =ms
        ostart =os
-       action "s") 
-  
-  (p clear-new-imaginal-chunk
+       action "s")
+
+(p results-no-act-if-lose
+     =goal>
+       isa game-state
+       state results
+       mresult lose
      ?imaginal>
-       state free
-       buffer full
-     ==>
-     -imaginal>
-     )
-  )
+       state   free
+    ==>
+     =goal>
+       state nil
+	   +imaginal>
+     isa learned-info
+       mstart nil
+       ostart nil
+       action nil)
+(p clear-new-imaginal-chunk
+    ?imaginal>
+      state free
+      buffer full
+    ==>
+    -imaginal>
+)
+
+)
