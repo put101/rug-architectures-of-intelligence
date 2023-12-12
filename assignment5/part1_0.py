32c3fbdf7a0940d0ae91dd699a9787422ec6252f
not-matthias <26800596+not-matthias@users.noreply.github.com>
2023-12-12 12:06:38 +0100

    feat: add part1

---
+++ assignment5/part1_0.py
@@ -0,0 +1,27 @@
+import nengo
+
+model = nengo.Network()
+with model:
+    
+    # Now build your own model, consisting of one stimulus node and three ensembles, a, b, and c:
+    #
+    stim = nengo.Node(0)
+    
+    a = nengo.Ensemble(n_neurons=100, dimensions=1)
+    b = nengo.Ensemble(n_neurons=100, dimensions=1)
+    c = nengo.Ensemble(n_neurons=100, dimensions=1)
+    
+    # Connect the stimulus and ensembles as follows:
+    # - stimulus to a is a simple connection without transformations or functions.
+    # - a to b calculates the square of a centered around 0: a*a - .5.
+    # - b to c is a connection that only gives half input to c, by specifying transform=.5.
+    # - make c into a simple memory by specifying a recurrent connection from c to c. 
+    #   As in tutorial 11, set the synaptic constant of this connection to 100 ms. 
+    #
+    def centered_square(x):
+        return x * x - .5;
+
+    nengo.Connection(stim, a)
+    nengo.Connection(a, b, function=centered_square)
+    nengo.Connection(b, c, transform=.5)
+    nengo.Connection(c, c, synapse=.1)
---
+++ assignment5/part1_1.py
@@ -0,0 +1,47 @@
+import nengo
+import numpy as np
+
+model = nengo.Network()
+with model:
+    # =================================================================================================
+    # Define the stimulus for the model as a sine wave, which 
+    # will can passed to the ensembles.
+    #
+    def sin_func(t):
+        return np.sin(2*t)
+    stim = nengo.Node(sin_func)
+    
+    # =================================================================================================
+    # Define the ensembles (=a group of neurons that collectively represent a vector) for the model.
+    # 
+    a = nengo.Ensemble(n_neurons=100, dimensions=1)
+    b = nengo.Ensemble(n_neurons=100, dimensions=1)
+    c = nengo.Ensemble(n_neurons=100, dimensions=1)
+    d = nengo.Ensemble(n_neurons=100, dimensions=2) # note: 2 dimensions, representing 2 values
+    
+    # =================================================================================================
+    # Connect the stimulus and ensembles:
+    # 
+
+    # Connect stimulus to a
+    nengo.Connection(stim, a)
+
+    # Connect stimulus to d[0] (first dimension of d)
+    nengo.Connection(stim, d[0])
+
+    # a to b calculates the square of a centered around 0
+    def centered_square(x):
+        return x * x - .5;
+    nengo.Connection(a, b, function=centered_square)
+
+    # b to c is a connection that only gives half input to c
+    nengo.Connection(b, c, transform=.5)
+
+    # c to c is a recurrent connection (simple memory)
+    nengo.Connection(c, c, synapse=.1)
+
+    # c to d[1] (second dimension of d)
+    nengo.Connection(c, d[1])
+
+    
+
